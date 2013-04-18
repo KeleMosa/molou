eca13461e138dccc133c75c2c5b877ba005dbe2a
Kelebogile <kelebogilemosa@gmail.com>
2013-04-18 17:14:06 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Molou::Application.config.secret_token = '2f09e13152accc6e2dd712d87da00a388225609a6f47db10def4a3d29766f7d9bef54b3a9c4b705142603cd33072bbfad987afd5a524d7af2e2d4a6ea6ed2a50'
